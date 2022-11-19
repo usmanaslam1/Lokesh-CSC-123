c573623e0e76036eaedfc90468624643ef5bc504
usmanaslam1 <usman@usman.uk>
2022-11-19 15:47:05 -0800

    my first commit

---
+++ cp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+while read p; do
+
+	result=$(curl -k -i --cookie ./cookie -d "action=post&actionType=submitURL&urlCategorization=$p" https://urlcat.checkpoint.com/urlcat/main.htm 2>/dev/null | grep -A4 "Categories:" | tr -d '\n' |  cut -d '/' -f2)
+
+echo $p,$result
+sleep 1
+done <sites.txt
